2137ad45c85c056f0f6a157464f65ef5a9cb5e40
TmTsimp <161818242+TmTsimp@users.noreply.github.com>
2025-05-13 19:56:00 +0800

    BottomNavigationBarItem 改用 TPSvgIcon

--- lib/util/tp_bottom_navigation_factory.dart
+++ lib/util/tp_bottom_navigation_factory.dart
@@ -4,23 +4,24 @@ import 'package:town_pass/page/bill/bill_view.dart';
 import 'package:town_pass/page/city_service/city_service_view.dart';
 import 'package:town_pass/page/home/home_view.dart';
 import 'package:town_pass/page/perk/perk_view.dart';
+import 'package:town_pass/util/extension/svg_gen_image.dart';
 
 class TPBottomNavigationFactory {
   static List<BottomNavigationBarItem> get barItemList => [
         BottomNavigationBarItem(
-          icon: Assets.svg.iconTabbarServiceDefault.svg(),
+          icon: Assets.svg.iconTabbarServiceDefault.icon(),
           label: '服務',
         ),
         BottomNavigationBarItem(
-          icon: Assets.svg.iconTabbarHomeDefault.svg(),
+          icon: Assets.svg.iconTabbarHomeDefault.icon(),
           label: '首頁',
         ),
         BottomNavigationBarItem(
-          icon: Assets.svg.iconTabbarCouponDefault.svg(),
+          icon: Assets.svg.iconTabbarCouponDefault.icon(),
           label: '優惠',
         ),
         BottomNavigationBarItem(
-          icon: Assets.svg.iconTabbarAccountDefault.svg(),
+          icon: Assets.svg.iconTabbarAccountDefault.icon(),
           label: '帳務',
         ),
       ];
