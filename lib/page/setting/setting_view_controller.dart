759db2540bf73007a8ad24911f107d1c516447ba
andywukoa <andywu@koafaith.com>
2025-08-13 12:19:53 +0800

    refactor: 使用 RxBool 簡化 Notification 與 BioAuth 的狀態管理。

--- lib/page/setting/setting_view_controller.dart
+++ lib/page/setting/setting_view_controller.dart
@@ -1,23 +1,16 @@
+dart
 import 'package:get/get.dart';
 
 class SettingViewController extends GetxController {
-  bool _isNotificationOn = true;
+  // 使用 RxBool 管理 notification 狀態
+  final RxBool _isNotificationOn = true.obs;
+  bool get isNotificationOn => _isNotificationOn.value;
+  set isNotificationOn(bool isOn) => _isNotificationOn.value = isOn;
 
-  bool get isNotificationOn => _isNotificationOn;
-
-  set isNotificationOn(bool isOn) {
-    _isNotificationOn = isOn;
-    update();
-  }
-
-  bool _isBioAuthOn = true;
-
-  bool get isBioAuthOn => _isBioAuthOn;
-
-  set isBioAuthOn(bool isOn) {
-    _isBioAuthOn = isOn;
-    update();
-  }
+  // 使用 RxBool 管理 bio authentication 狀態
+  final RxBool _isBioAuthOn = true.obs;
+  bool get isBioAuthOn => _isBioAuthOn.value;
+  set isBioAuthOn(bool isOn) => _isBioAuthOn.value = isOn;
 
   DateTime get lastLoginDateTime => DateTime.now();
 }
