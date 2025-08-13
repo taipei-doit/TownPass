872506af8480a9e8d78bd80c69ad201dfb254e09
andywukoa <andywu@koafaith.com>
2025-08-13 12:02:42 +0800

    refactor: 優化 TPSvgIcon，使用 LayoutBuilder 使 icon size 更靈活，避免繼承 IconTheme。

--- lib/util/tp_svg_icon.dart
+++ lib/util/tp_svg_icon.dart
@@ -1,31 +1,38 @@
+dart
 import 'package:flutter/material.dart';
 import 'package:town_pass/gen/assets.gen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 /// Make simple svg icon widget, form Assets.svg.{icon}.svg, honors
 /// IconTheme's color and size attribute.
 class TPSvgIcon extends StatelessWidget {
   final SvgGenImage icon;
   final String? semanticsLabel;
+  final double? size;
+  final Color? color;
 
   const TPSvgIcon({
     super.key,
     required this.icon,
     this.semanticsLabel,
+    this.size,
+    this.color,
   });
 
   @override
   Widget build(BuildContext context) {
-    final IconThemeData iconTheme = IconTheme.of(context);
-    return icon.svg(
-      semanticsLabel: semanticsLabel,
-      height: iconTheme.size,
-      width: iconTheme.size,
-      colorFilter: switch (iconTheme.color) {
-        Color color => ColorFilter.mode(
-            color,
-            BlendMode.srcIn,
-          ),
-        null => null,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final iconSize = size ?? constraints.biggest.shortestSide;
+        return SvgPicture.asset(
+          icon.path,
+          semanticsLabel: semanticsLabel,
+          height: iconSize,
+          width: iconSize,
+          colorFilter: color != null
+              ? ColorFilter.mode(color!, BlendMode.srcIn)
+              : null,
+        );
       },
     );
   }
