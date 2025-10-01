4b980691e565d6d47d91773fbb1f2be747114b4a
stanleyfish0813 <stanley.fish.0813@gmail.com>
2025-10-01 10:10:39 +0000

    refactor: 將 `onInit` 中的 `Timer.periodic` 改為 `Future.delayed` 以清晰表達延遲載入。

--- lib/page/home/widget/news/news_banner_widget_controller.dart
+++ lib/page/home/widget/news/news_banner_widget_controller.dart
@@ -1,3 +1,4 @@
+# 下面放重構後的程式碼，不要用 ``` 包
 import 'dart:async';
 import 'dart:convert';
 
@@ -34,10 +35,12 @@ class NewsBannerWidgetController extends GetxController {
   void onInit() {
     super.onInit();
 
-    Timer.periodic(
+    // 原始程式碼使用 Timer.periodic 後立即取消，效果等同於一次性的延遲執行。
+    // 為了提升程式碼的可讀性與語義的清晰度，已將其重構為使用 Future.delayed，
+    // 同時保留了原有的 3 秒延遲載入行為。
+    Future.delayed(
       const Duration(seconds: 3),
-      (timer) async {
-        timer.cancel();
+      () async {
         final String banners = await rootBundle.loadString(Assets.mockData.homeBanner);
         list = HomeBannerList.fromJson(jsonDecode(banners));
         isLoading = false;
