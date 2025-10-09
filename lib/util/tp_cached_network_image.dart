a5ba70dcab7c48e0652f8e2a1852f8399d409303
stanleyfish0813 <stanley.fish.0813@gmail.com>
2025-10-09 15:54:25 +0800

    refactor: 移除多餘程式碼

--- lib/util/tp_cached_network_image.dart
+++ lib/util/tp_cached_network_image.dart
@@ -32,24 +32,23 @@ class TPCachedNetworkImage extends StatelessWidget {
     return Container(
       width: width,
       height: height,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(borderRadius),
-        child: Container(
-          color: color, // 此顏色將作為圓角區域內圖片的背景色
-          child: CachedNetworkImage(
-            imageUrl: imageUrl,
-            httpHeaders: httpHeaders,
-            width: double.infinity,
-            height: double.infinity,
-            fit: fit,
-            placeholder: (context, string) =>
-                const Center(child: CircularProgressIndicator()),
-            errorWidget: (context, string, object) => ColoredBox(
-              color: TPColors.grayscale100, // 錯誤狀態下的背景色，也會被 ClipRRect 剪裁
-              child: Center(
-                child: Assets.svg.logoS.svg(),
-              ),
-            ),
+      decoration: ShapeDecoration(
+        color: color,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius)),
+      ),
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        httpHeaders: httpHeaders,
+        width: double.infinity,
+        height: double.infinity,
+        fit: fit,
+        placeholder: (context, string) =>
+            const Center(child: CircularProgressIndicator()),
+        errorWidget: (context, string, object) => ColoredBox(
+          color: TPColors.grayscale100,
+          child: Center(
+            child: Assets.svg.logoS.svg(),
           ),
         ),
       ),
