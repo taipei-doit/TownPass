5be0b986b4d4f62f9b5494117d8b8e05e4e35099
stanleyfish0813 <stanley.fish.0813@gmail.com>
2025-10-20 02:00:39 +0000

    refactor: 將 Language 列舉的顯示字串邏輯移至列舉本身定義，提升結構與可讀性。

--- lib/page/language/language_view.dart
+++ lib/page/language/language_view.dart
@@ -5,14 +5,10 @@ import 'package:town_pass/util/tp_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-extension on Language {
-  String get string {
-    return switch (this) {
-      Language.traditionalChinese => '繁體中文',
-      Language.english => 'English',
-    };
-  }
-}
+// 原有的 Language 列舉擴充功能 (extension on Language) 已從此檔案移除，
+// 並將其定義移至 `language_view_controller.dart` 中的 `Language` 列舉定義內。
+// 這樣能將列舉的顯示邏輯與列舉本身共同維護，提升程式碼的結構與可讀性，
+// 且當新增語言選項時，只需在 `Language` 列舉中一併更新即可。
 
 class LanguageView extends GetView<LanguageViewController> {
   const LanguageView({super.key});
