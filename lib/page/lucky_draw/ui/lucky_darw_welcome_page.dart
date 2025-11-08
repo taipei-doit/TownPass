7c6586838d441d70257933c9e1cca46115938c97
112062371 <bbacnt0925@gmail.com>
2025-11-08 11:52:55 +0800

    add: shake feature

--- lib/page/lucky_draw/ui/lucky_darw_welcome_page.dart
+++ lib/page/lucky_draw/ui/lucky_darw_welcome_page.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:town_pass/util/tp_app_bar.dart';
 import 'package:town_pass/util/tp_colors.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class LuckyDarwWelcomePage extends StatefulWidget {
   const LuckyDarwWelcomePage({super.key});
@@ -10,6 +13,35 @@ class LuckyDarwWelcomePage extends StatefulWidget {
 }
 
 class _LuckyDarwWelcomePageState extends State<LuckyDarwWelcomePage> {
+  final _streamSubscription = <StreamSubscription<dynamic>>[];
+
+  _shakeListen() {
+    const val = 15.0;
+    _streamSubscription
+        .add(userAccelerometerEventStream().listen((userAccelerometerEvent) {
+      if (userAccelerometerEvent.x.abs() >= val ||
+          userAccelerometerEvent.y.abs() >= val ||
+          userAccelerometerEvent.z.abs() >= val) {
+        // Detected a shake
+        print('Device shaken!');
+      }
+    }));
+  }
+
+  @override
+  void initState() {
+    _shakeListen();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    for (final subscription in _streamSubscription) {
+      subscription.cancel();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
