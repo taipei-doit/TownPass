32534d3809a48c0659aaa6bca0f4ee005e5e0eed
SquidSpirit <squid@squidspirit.com>
2025-11-09 00:03:28 +0800

    feat: replace dialog with bottom sheet for temple info display and update padding for better layout

--- lib/page/lucky_draw/ui/temple_page.dart
+++ lib/page/lucky_draw/ui/temple_page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:town_pass/models/attraction.dart';
+import 'package:get/route_manager.dart';
 import 'package:town_pass/page/lucky_draw/data/temple.dart';
 import 'package:town_pass/page/lucky_draw/ui/lucky_draw_app_bar.dart';
 import 'package:town_pass/util/tp_button.dart';
 import 'package:town_pass/util/tp_colors.dart';
 import 'package:town_pass/util/tp_text.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class TemplePage extends StatefulWidget {
   const TemplePage({super.key});
@@ -66,21 +65,17 @@ class _TemplePageState extends State<TemplePage> {
         ),
         child: InkWell(
           onTap: () {
-            // showModalBottomSheet(
-            //   context: context,
-            //   isScrollControlled: true,
-            //   isDismissible: true,
-            //   backgroundColor: TPColors.white,
-            //   shape: const RoundedRectangleBorder(
-            //     borderRadius: BorderRadius.vertical(
-            //       top: Radius.circular(24),
-            //     ),
-            //   ),
-            //   builder: (_) => _bottomSheet(temple),
-            // );
-            showDialog(
+            showModalBottomSheet(
               context: context,
-              builder: (context) => _templeInfoDialog(context, temple),
+              isScrollControlled: true,
+              isDismissible: true,
+              backgroundColor: TPColors.white,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(24),
+                ),
+              ),
+              builder: (_) => _bottomSheet(temple),
             );
           },
           borderRadius: BorderRadius.circular(12),
@@ -109,7 +104,7 @@ class _TemplePageState extends State<TemplePage> {
               ),
               Container(
                 width: double.infinity,
-                padding: const EdgeInsets.all(12.0),
+                padding: const EdgeInsets.all(16),
                 decoration: const BoxDecoration(
                   borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(12),
@@ -141,95 +136,63 @@ class _TemplePageState extends State<TemplePage> {
     );
   }
 
-  AlertDialog _templeInfoDialog(BuildContext context, Temple temple) {
-    return AlertDialog(
-      title: Text(temple.name),
-      content: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "${temple.city} ${temple.district}",
-              style: const TextStyle(color: TPColors.grayscale400),
-            ),
-            const SizedBox(height: 8),
-            const SizedBox(height: 8),
-            if (temple.introduction.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(temple.introduction),
+  Widget _bottomSheet(Temple temple) => DraggableScrollableSheet(
+        expand: false,
+        initialChildSize: 0.5, // 初始高度
+        minChildSize: 0.3, // 最小高度
+        maxChildSize: 0.9, // 最大高度
+        builder: (context, scrollController) {
+          return SingleChildScrollView(
+            controller: scrollController,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Container(
+                      width: 50,
+                      height: 5,
+                      margin: const EdgeInsets.only(bottom: 12),
+                      decoration: BoxDecoration(
+                        color: TPColors.grayscale300,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: TPText(
+                      temple.name,
+                      style: const TPTextStyles(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  TPText(
+                    '${temple.city} ${temple.district}',
+                    style: const TPTextStyles(fontSize: 16),
+                  ),
+                  const SizedBox(height: 16),
+                  TPText(
+                    temple.introduction,
+                    style: const TPTextStyles(fontSize: 16),
+                  ),
+                  const SizedBox(height: 24),
+                  Center(
+                    child: TPButton(
+                      text: '線上參拜',
+                      onPressed: () =>
+                          Get.toNamed('/lucky_draw/incense_burning'),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                ],
               ),
-          ],
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: const Text('Close'),
-        ),
-      ],
-    );
-  }
-
-  Widget _bottomSheet(Attraction attraction) => DraggableScrollableSheet(
-      expand: false,
-      initialChildSize: 0.5, // 初始高度
-      minChildSize: 0.3, // 最小高度
-      maxChildSize: 0.9, // 最大高度
-      builder: (context, scrollController) {
-        return SingleChildScrollView(
-          controller: scrollController,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Container(
-                    width: 50,
-                    height: 5,
-                    margin: const EdgeInsets.only(bottom: 12),
-                    decoration: BoxDecoration(
-                      color: TPColors.grayscale300,
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8),
-                  child: TPText(
-                    attraction.name,
-                    style: const TPTextStyles(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 8),
-                TPText(attraction.address),
-                const SizedBox(height: 16),
-                TPText(
-                  attraction.introduction,
-                  style: const TPTextStyles(fontSize: 16),
-                ),
-                const SizedBox(height: 24),
-                Center(
-                  child: TPButton(
-                    text: '前往查看詳情',
-                    onPressed: () async {
-                      final uri = Uri.parse(attraction.url);
-                      if (await canLaunchUrl(uri)) {
-                        await launchUrl(uri,
-                            mode: LaunchMode.externalApplication);
-                      }
-                    },
-                  ),
-                ),
-                const SizedBox(height: 16),
-              ],
             ),
-          ),
-        );
-      },
-    );
+          );
+        },
+      );
 }
