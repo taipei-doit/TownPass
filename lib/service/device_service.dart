103b969b6f3295378c68625241592098815f4aaa
b11902088@ntu.edu.tw <xiedienhua@gmail.com>
2025-11-08 11:17:50 +0800

    Fixed demonstration issue

--- lib/service/device_service.dart
+++ lib/service/device_service.dart
@@ -1,24 +1,30 @@
-import 'dart:io';
-
+import 'package:flutter/foundation.dart' show kIsWeb; 
 import 'package:device_info_plus/device_info_plus.dart';
+import 'dart:io'; 
+
 import 'package:get/get.dart';
 
 class DeviceService extends GetxService {
   AndroidDeviceInfo? androidDeviceInfo;
   IosDeviceInfo? iosDeviceInfo;
+  WebBrowserInfo? webBrowserInfo; 
   BaseDeviceInfo? baseDeviceInfo;
 
   Future<DeviceService> init() async {
     final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
-    baseDeviceInfo = await deviceInfo.deviceInfo;
+    if (kIsWeb) {
+      webBrowserInfo = await deviceInfo.webBrowserInfo;
+      baseDeviceInfo = webBrowserInfo; 
+    } else {
 
-    if (Platform.isAndroid) {
-      androidDeviceInfo = await deviceInfo.androidInfo;
-    }
-
-    if (Platform.isIOS) {
-      iosDeviceInfo = await deviceInfo.iosInfo;
+      if (Platform.isAndroid) {
+        androidDeviceInfo = await deviceInfo.androidInfo;
+        baseDeviceInfo = androidDeviceInfo; 
+      } else if (Platform.isIOS) {
+        iosDeviceInfo = await deviceInfo.iosInfo;
+        baseDeviceInfo = iosDeviceInfo; 
+      }
     }
     return this;
   }
