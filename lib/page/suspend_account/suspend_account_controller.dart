4ec5c023538f6aea2ba2ebb1aaca544ad7c68508
stanleyfish0813 <stanley.fish.0813@gmail.com>
2026-03-23 02:00:19 +0000

    refactor: 快取 AccountService 實例，避免重複查找，提升效能。

--- lib/page/suspend_account/suspend_account_controller.dart
+++ lib/page/suspend_account/suspend_account_controller.dart
@@ -3,7 +3,10 @@ import 'package:town_pass/service/account_service.dart';
 import 'package:get/get.dart';
 
 class SuspendAccountController extends GetxController {
-  Account? get account => Get.find<AccountService>().account;
+  // 快取 AccountService 實例，避免每次存取 account 時都重複查找服務
+  final AccountService _accountService = Get.find<AccountService>();
+
+  Account? get account => _accountService.account;
 
   String get name => account?.realName ?? '';
 
