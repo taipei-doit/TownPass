d702487bb418b9d722b0c792cd3ad0e755bca5f0
stanleyfish0813 <stanley.fish.0813@gmail.com>
2026-06-01 02:00:38 +0000

    refactor: 抽取富文本樣式計算邏輯至獨立私有方法，提升可讀性與結構。

--- lib/page/message_detail/message_detail_view.dart
+++ lib/page/message_detail/message_detail_view.dart
@@ -1,3 +1,4 @@
+```
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -77,37 +78,14 @@ class _RichText extends StatelessWidget {
     return TPText.rich(_rich(richText));
   }
 
+  /// Generates a TextSpan for a list of TPRichText objects, applying appropriate styles and tap recognizers.
   TextSpan _rich(List<TPRichText> richText) {
     return TextSpan(
       children: richText
           .map(
             (object) => TextSpan(
               text: object.text ?? object.url,
-              style: switch (object.style?.contains(TPRichTextStyle.bold)) {
-                true => TPTextStyles.h3SemiBold,
-                _ => TPTextStyles.h3Regular,
-              }
-                  .copyWith(
-                color: switch (object.url) {
-                  String() => TPColors.primary500,
-                  null => TPColors.grayscale800,
-                },
-                decoration: switch (object.url) {
-                  String() => TextDecoration.underline,
-                  null => switch (object.style?.contains(TPRichTextStyle.underline)) {
-                      true => TextDecoration.underline,
-                      _ => TextDecoration.none,
-                    }
-                },
-                decorationColor: switch (object.url) {
-                  String() => TPColors.primary500,
-                  null => TPColors.grayscale800,
-                },
-                fontStyle: switch (object.style?.contains(TPRichTextStyle.italic)) {
-                  true => FontStyle.italic,
-                  _ => FontStyle.normal,
-                },
-              ),
+              style: _getTextStyle(object), // 抽取複雜的樣式判斷邏輯到獨立方法
               recognizer: switch (object.url) {
                 String uri => TapGestureRecognizer()
                   ..onTap = () async => await TPRoute.openUri(
@@ -120,4 +98,47 @@ class _RichText extends StatelessWidget {
           .toList(),
     );
   }
+
+  /// Calculates the TextStyle for a given TPRichText object.
+  ///
+  /// This method encapsulates the logic for determining font weight, color,
+  /// decoration, decoration color, and font style based on the TPRichText
+  /// properties like URL and contained styles (bold, underline, italic).
+  TextStyle _getTextStyle(TPRichText object) {
+    // 根據是否包含粗體樣式來決定基礎字體樣式
+    TextStyle baseStyle = switch (object.style?.contains(TPRichTextStyle.bold)) {
+      true => TPTextStyles.h3SemiBold,
+      _ => TPTextStyles.h3Regular,
+    };
+
+    // 根據是否為 URL 來決定文字顏色
+    final Color textColor = switch (object.url) {
+      String() => TPColors.primary500,
+      null => TPColors.grayscale800,
+    };
+
+    // 根據是否為 URL 或是否包含底線樣式來決定文字裝飾
+    final TextDecoration textDecoration = switch (object.url) {
+      String() => TextDecoration.underline,
+      null => switch (object.style?.contains(TPRichTextStyle.underline)) {
+        true => TextDecoration.underline,
+        _ => TextDecoration.none,
+      }
+    };
+
+    // 根據是否包含斜體樣式來決定字體風格
+    final FontStyle fontStyle = switch (object.style?.contains(TPRichTextStyle.italic)) {
+      true => FontStyle.italic,
+      _ => FontStyle.normal,
+    };
+
+    // 合併所有計算出的樣式
+    return baseStyle.copyWith(
+      color: textColor,
+      decoration: textDecoration,
+      decorationColor: textColor, // 裝飾顏色與文字顏色相同
+      fontStyle: fontStyle,
+    );
+  }
 }
+```
