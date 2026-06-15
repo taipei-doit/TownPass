615fe21d950fbf453bf3ca9e7ce9d2aaa70053b6
stanleyfish0813 <stanley.fish.0813@gmail.com>
2026-06-15 02:00:26 +0000

    refactor: 提取魔術數字為常數，提升可讀性與維護性。

--- lib/page/message/widget/_empty_message_widget.dart
+++ lib/page/message/widget/_empty_message_widget.dart
@@ -3,19 +3,25 @@ part of 'package:town_pass/page/message/message_view.dart';
 class _EmptyMessageWidget extends StatelessWidget {
   const _EmptyMessageWidget();
 
+  // 定義相關尺寸與間距常數，提升程式碼可讀性與維護性
+  static const double _imageSize = 160.0;
+  static const double _verticalPadding = 8.0;
+  static const int _topSpacerFlex = 2;
+  static const int _bottomSpacerFlex = 3;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        const Spacer(flex: 2),
+        const Spacer(flex: _topSpacerFlex),
         SizedBox(
-          width: 160,
-          height: 160,
+          width: _imageSize,
+          height: _imageSize,
           child: Center(child: Assets.svg.bookingL.svg()),
         ),
         const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8),
+          padding: EdgeInsets.symmetric(vertical: _verticalPadding),
           child: TPText(
             '目前無訊息',
             style: TPTextStyles.h2SemiBold,
@@ -27,7 +33,7 @@ class _EmptyMessageWidget extends StatelessWidget {
           style: TPTextStyles.bodySemiBold,
           color: TPColors.grayscale400,
         ),
-        const Spacer(flex: 3),
+        const Spacer(flex: _bottomSpacerFlex),
       ],
     );
   }
